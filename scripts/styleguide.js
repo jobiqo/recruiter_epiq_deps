3539f0e8e354c61ac64478a896c02b333fb49562
Joao Garin <joao.garin@gmail.com>
2016-09-22 16:01:28 +0200

    fix(styleguide): fix styleguide vars

--- scripts/styleguide.js
+++ scripts/styleguide.js
@@ -1,7 +1,6 @@
 var exec = require('child_process').exec,
-  epiq_dir = '../../../../../../profiles/recruiter/themes/epiq';
-
-var cmd = 'cd node_modules/recruiter_epiq_deps && gulp styleguide-kss --epiq_dir ' + epiq_dir;
+  epiq_dir = '../../../../../../profiles/recruiter/themes/epiq'
+  cmd = 'cd node_modules/recruiter_epiq_deps && gulp styleguide-kss --epiq_dir ' + epiq_dir;
 
 exec(cmd, function (error, stdout, stderr) {
   // command output is in stdout
