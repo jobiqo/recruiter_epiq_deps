c12ca3e16f577c0a5f058dfadf8e62502cdd913d
Joao Garin <joao.garin@gmail.com>
2017-12-14 14:52:25 +0100

    fix(critial-css): Accept param and bypass prompt

--- scripts/critical.js
+++ scripts/critical.js
@@ -1,8 +1,30 @@
 var exec = require('child_process').exec,
-  chalk = require('chalk'),
-  epiq_dir = '../../../../../../profiles/recruiter/themes/epiq',
-  gulpconfig = '../../gulpconfig.json',
-  args = "";
+chalk = require('chalk'),
+epiq_dir = '../../../../../../profiles/recruiter/themes/epiq',
+gulpconfig = '../../gulpconfig.json',
+args = "",
+yargs = require('yargs').argv;
+
+console.log(yargs);
+
+if (yargs.site) {
+args = '--site ' + yargs.site;
+
+var cmd = 'cd node_modules/recruiter_epiq_deps && gulp critical-css --epiq_dir ' + epiq_dir + ' --gulpconfig ' + gulpconfig + ' ' + args;
+
+console.log('Compiling critical css - ' + chalk.yellow('gulp critical-css'));
+exec(cmd, function (error, stdout, stderr) {
+  // command output is in stdout
+  if (!error) {
+    console.log(chalk.green(stdout));
+  }
+  else {
+    console.log(chalk.white(stdout));
+    console.log(chalk.red(error));
+  }
+  console.log(chalk.red(stderr));
+});
+} else {
 
 var prompt = require('prompt');
 prompt.start();
@@ -26,3 +48,4 @@ prompt.get(['site'], function (err, result) {
     console.log(chalk.red(stderr));
   });
 });
+}
